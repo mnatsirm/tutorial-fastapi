103e3b6cce001d49c7f4c67d17102e28a79fb385
mnatsirm <mnatsirm53@gmail.com>
2021-03-08 23:53:24 +0700

    multiple body parameters

--- main.py
+++ main.py
@@ -223,32 +223,81 @@ async def get_model(model_name: ModelName):
     ####################################
 
     1. Starting point
-    from fastapi import FastAPI, Path
-    from typing import Optional
-    from pydantic import BaseModel
+        from fastapi import FastAPI, Path
+        from typing import Optional
+        from pydantic import BaseModel
 
-    app = FastAPI()
-
-
-    class Item(BaseModel):
-        name: str
-        description: Optional[str] = None
-        price: float
-        tax: Optional[float] = None
+        app = FastAPI()
 
 
-    @app.put("/items/{item_id}")
-    async def update_item(
-        *,
-        item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
-        q: Optional[str] = None,
-        item: Optional[Item] = None,
-    ):
-        results = {"item_id": item_id}
-        if q:
-            results.update({"q": q})
-        if item:
-            results.update({"item": item})
-        return results
+        class Item(BaseModel):
+            name: str
+            description: Optional[str] = None
+            price: float
+            tax: Optional[float] = None
 
+
+        @app.put("/items/{item_id}")
+        async def update_item(
+            *,
+            item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
+            q: Optional[str] = None,
+            item: Optional[Item] = None,
+        ):
+            results = {"item_id": item_id}
+            if q:
+                results.update({"q": q})
+            if item:
+                results.update({"item": item})
+            return results
+
+        Path operations would expect JSON body with the attribute of an Item, like
+        {
+            "name": "Foo",
+            "description": "The pretender",
+            "price": 42.0,
+            "tax": 3.2
+        }
+
+    
+    2. Multiple body parameters
+        Dude i love fastapi design, this is epic, when dealing multiple body parameter,
+        just declare it, and add it on function api..
+        The code:
+            from fastapi import FastAPI
+            from typing import Optional
+            from pydantic import BaseModel
+
+            app = FastAPI()
+
+            class Item(BaseModel):
+                name: str
+                description: Optional[str] = None
+                price: float
+                tax: Optional[float] = None
+
+
+            class User(BaseModel):
+                username: str
+                full_name: Optional[str] = None
+
+
+            @app.put("/items/{item_id}")
+            async def update_item(item_id: int, item: Item, user: User):
+                results = {"item_id": item_id, "item": item, "user": user}
+                return results
+
+        Path operations would expect JSON body with the attribute of an Item, like
+        {
+            "item": {
+                "name": "Foo",
+                "description": "The pretender",
+                "price": 42.0,
+                "tax": 3.2
+            },
+            "user": {
+                "username": "dave",
+                "full_name": "Dave Grohl"
+            }
+        }
 """

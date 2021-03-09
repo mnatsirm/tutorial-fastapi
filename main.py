dd81cab1de864658eba9f7c33eb19ba04a9c5a7d
mnatsirm <mnatsirm53@gmail.com>
2021-03-09 17:37:39 +0700

    singular values in body

--- main.py
+++ main.py
@@ -300,4 +300,46 @@ async def get_model(model_name: ModelName):
                 "full_name": "Dave Grohl"
             }
         }
+    
+    3. Singular values in Body
+        The code:
+            from fastapi import Body, FastAPI
+            from typing import Optional
+            from pydantic import BaseModel
+
+            app = FastAPI()
+
+            class Item(BaseModel):
+                name: str
+                description: Optional[str] = None
+                price: float
+                tax: Optional[float] = None
+
+            class User(BaseModel):
+                username: str
+                full_name: Optional[str] = None
+
+            @app.put("/items/{item_id}")
+            async def update_item(
+                item_id: int, item: Item, user: User, importance: int = Body(...)
+            ):
+                results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+                return results
+
+        FastAPI will expect body like:
+        {
+            "item": {
+                "name": "Foo",
+                "description": "The pretender",
+                "price": 42.0,
+                "tax": 3.2
+            },
+            "user": {
+                "username": "dave",
+                "full_name": "Dave Grohl"
+            },
+            "importance": 5
+        }
+
+
 """
